a62066828ecfa404f0fea9570468f708f0899674
unknown <jair.lopes@fatec.sp.gov.br>
2022-09-13 22:06:45 -0300

    Subindo API para o Heroku

--- src/main/java/com/fatec/crudbancodedadosnaorelacional/domain/entities/ComponentEntity.java
+++ src/main/java/com/fatec/crudbancodedadosnaorelacional/domain/entities/ComponentEntity.java
@@ -1,26 +1,35 @@
 package com.fatec.crudbancodedadosnaorelacional.domain.entities;
 
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
 import com.vladmihalcea.hibernate.type.json.JsonType;
 import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "tb_component")
 @Data
-@TypeDef(
-        name = "json",
-        typeClass = JsonType.class
-)
+//@TypeDef(
+//        name = "json",
+//        typeClass = JsonType.class
+//)
+@TypeDefs({
+        @TypeDef(name = "jsonb", typeClass = JsonBinaryType.class),
+})
 public class ComponentEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name = "increment", strategy = "increment")
+    @Column(name = "id")
     private Long id;
 
-    @Type(type = "json")
+    @Type(type = "jsonb")
     @Column(name = "component", columnDefinition = "json")
     private String component;
 }
